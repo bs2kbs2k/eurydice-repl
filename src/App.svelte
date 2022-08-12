9ea8c7159c907b342291bd391e0bdd4454fc97bd
bs2k <bs2k@bs2k.me>
2022-08-12 17:12:54 +0900

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import icon from './assets/eurydice.svg';
+  import evaluate from 'eurydice/src/evaluate';
+  import parse from 'eurydice/src/parse';
+  import printValue from 'eurydice/src/print';
+  let expr = "...4d20";
+  function evalString(expr: string): string {
+    try {
+      return printValue(evaluate(parse(expr)));
+    } catch (e) {
+      return e;
+    }
+  }
+</script>
+
+<main>
+  <img src={icon} alt="Eurydice icon">
+  <h1>Try Eurydice</h1>
+  <input type="text" bind:value={expr} autocapitalize="none" autocomplete="none" autocorrect="none">
+  <div>{evalString(expr)}</div>
+</main>
